cf5f4b4b2a6b753882bac18dfcd9df606623ae0f
JeongHun Kim <JeongHun Kim>
2018-07-17 22:09:37 +0900

    blog.models.Post aㅗ델에 대한 PostAdmin 커스텀

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from . models import Post
 
-admin.site.register(Post)
+
+# admin.ModelAdmin
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):\
+    list_display = ['id', 'title', 'created_at', 'updated_at']
+
+
+
+# admin.site.register(Post, PostAdmin)
+
--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,8 @@ def lnglat_validator(value):
         raise ValidationError('Invalid LngLat Type')
 
 class Post(models.Model):
-    name = models.CharField(max_length=100, verbose_name='제목',
+    author = models.CharField(max_length=20)
+    title = models.CharField(max_length=100, verbose_name='제목',
         help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.')
     content = models.TextField(verbose_name='내용')
     tage = models.CharField(max_length=100, blank=True)
